fc04b654f75bb5c05a8f9b60c0ca4d9c9fb92d3e
Pesi <pedro.suarez.miranda@gmail.com>
2020-05-08 13:40:55 +0100

    Añado el proyecto GenerateClassificationTableReport, y lo dejo funcionando.

---
+++ GenerateClassificationTableReport/GenerateClassificationTableReport.cs
@@ -0,0 +1,28 @@
+﻿using Quinix;
+using Quinix.Data;
+using Quinix.Model;
+using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        //Adjust this values to get the desired classification table.
+        int seasonYear = 2016;
+        string divisionName = "primera";
+        string reportFilePath = @"..\..\..\output\classification_table.csv";
+
+        QuinixDbContext context = new QuinixDbContext();
+        Season season = context.Seasons.Single(s => s.StartYear == seasonYear);
+        Division division = context.Divisions.Single(d => d.Name.Equals(divisionName));
+        ClassificationTable table = new ClassificationTable(season, division, 1, 38, 38);
+        table.GenerateClassificationTable();
+        ReportGenerator reportGenerator = new ReportGenerator(table.History, reportFilePath);
+        reportGenerator.GenerateReport(38);
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+}
