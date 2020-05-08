36635739f29185e0a0136fc85dfb838abbbc33f0
Pesi <pedro.suarez.miranda@gmail.com>
2020-05-08 12:45:27 +0100

    Añado migración para añadir automáticamente las divisiones
    El resto de la BD la rellana el web scrapper, pero esta parte no.

---
+++ Quinix/Migrations/20200508114105_InsertDivisions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quinix.Migrations
+{
+    public partial class InsertDivisions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Divisions(Name, Number) VALUES ('primera', 1), ('segunda', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Divisions WHERE Name IN ('primera', 'segunda')");
+        }
+    }
+}
