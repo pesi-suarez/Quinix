7aa07dc0b212e0303863d5a2fd61bd8c9e226a17
Pesi <pedro.suarez.miranda@gmail.com>
2020-05-08 17:11:58 +0100

    Añado la aplicación de consola GenerateFeatureMatrix, que funciona correctamente.

---
+++ GenerateFeatureMatrix/GenerateFeatureMatrix.cs
@@ -0,0 +1,16 @@
+﻿using Quinix;
+using System.Globalization;
+
+class Program
+{
+    //Will generate the feature matrix associated to all the football games present in the database.
+    //May take up to 5 hours to finish when considering all games from season 2001-02 until now
+    public static void Main()
+    {
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-us");
+        FeatureMatrixGenerator featureMatrixGenerator = new FeatureMatrixGenerator(@"..\..\..\output\features.csv");
+        featureMatrixGenerator.Execute();
+        featureMatrixGenerator.PrintFeatureMatrix();
+    }
+
+}
