892e5b3d9d566a7b4a5b1587c417750487bd86d7
Pesi <pedro.suarez.miranda@gmail.com>
2020-05-11 12:42:16 +0100

    Añado tests para el scrapper web y para ReportGenerator

---
+++ QuinixUnitTesting/TestFootballMatchesWebScrapper.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Quinix;
+using System.IO;
+using System.Linq;
+using Quinix.Data;
+
+namespace QuinixUnitTesting
+{
+    public class TestFootballMatchesWebScrapper
+    {
+        [Fact]
+        public void WebScrapperToFile2016_2017()
+        {
+            string resultsFilePath = @"..\..\..\test_data\TestFootballMatchesWebScrapper\test_results.txt";
+            string comparisonFilePath = @"..\..\..\test_data\TestFootballMatchesWebScrapper\comparison_results.txt";
+            if (File.Exists(resultsFilePath))
+                File.Delete(resultsFilePath);
+            FootballMatchesWebScrapper mrp = new FootballMatchesWebScrapper(2016, 2016, NodeOperation.WriteToFile, resultsFilePath);
+            mrp.ExecuteAll();
+
+            Assert.Equal(Utils.GenerateFileHash(resultsFilePath), Utils.GenerateFileHash(comparisonFilePath));
+        }
+
+        [Fact]
+        public void WebScrapperToDb2016_2017()
+        {
+            //TODONEW: Probar a instalar y ejecutar el proyecto desde cero.
+            //TODONEW: Eliminar o traducir los comentarios -> Los comentarios que quedan son relevantes. Hay que traducirlos.
+            //NOTA: Debe usarse la BD de test (ver app.config)
+            QuinixDbContext context = new QuinixDbContext();
+            var matches = context.Matches;
+            foreach (var match in matches)
+            {
+                context.Remove(match);
+            }
+            context.SaveChanges();
+            Assert.Equal(0, context.Matches.Count());
+
+            FootballMatchesWebScrapper mrp = new FootballMatchesWebScrapper(2016, 2016, NodeOperation.SaveToDb, null);
+            mrp.ExecuteAll();
+
+            //Deben haberse creado 841 registros.
+            Assert.Equal(841, context.Matches.Count());
+
+            //Si vuelvo a ejecutar, debe seguir habiendo el mismo número de registros.
+            mrp.ExecuteAll();
+            Assert.Equal(841, context.Matches.Count());
+        }
+
+    }
+}
---
+++ QuinixUnitTesting/TestReportGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using System.Linq;
+using Quinix.Data;
+using Quinix.Model;
+using Quinix;
+
+namespace QuinixUnitTesting
+{
+    public class TestReportGenerator
+    {
+        [Fact]
+        public void ExecuteSingle()
+        {
+            //NOTE: DB should be populated for season 2016-17 before executing the test.
+
+            string resultsFilePath = @"..\..\..\test_data\TestReportGenerator\results.csv";
+            string comparisonFilePath = @"..\..\..\test_data\TestReportGenerator\classification_comparison.csv";
+
+            QuinixDbContext context = new QuinixDbContext();
+            Season season = context.Seasons.Single(s => s.StartYear == 2016);
+            Division division = context.Divisions.Single(d => d.Name.Equals("primera"));
+            ClassificationTable table = new ClassificationTable(season, division, 1, 38, 38);
+            table.GenerateClassificationTable();
+            ReportGenerator reportGenerator = new ReportGenerator(table.History, resultsFilePath);
+            reportGenerator.GenerateReport(38);
+
+            Assert.Equal(Utils.GenerateFileHash(resultsFilePath), Utils.GenerateFileHash(comparisonFilePath));
+        }
+
+    }
+}
